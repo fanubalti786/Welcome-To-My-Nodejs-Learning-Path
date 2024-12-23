9a4546ea39cf4218e9edac17e45a677e7f673722
fanubalti786 <fanubalti786@gmail.com>
2024-12-23 16:13:47 +0500

    Metarial Ui step complete with tailwind

--- TestRestApis/class01/src/App.jsx
+++ TestRestApis/class01/src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@material-tailwind/react'
 import React from 'react'
 
 export default function App() {
   return (
-    <div className="bg-red-500 text-white p-4">
+    <div className="bg-red-500 text-white p-4 flex">
     <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+    <Button>Click</Button>
   </div>
   )
 }
--- TestRestApis/class01/tailwind.config.js
+++ TestRestApis/class01/tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
