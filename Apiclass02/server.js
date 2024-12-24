54f4068cc93286eb9ea376b9eaa2b8e693ca6e2c
fanubalti786 <fanubalti786@gmail.com>
2024-12-25 03:32:34 +0500

    how we can recive Query Params through frent end but know i'm using postman agaist proper frentend

---
+++ Apiclass02/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 9000;
+
+app.listen(port,()=>
+{
+    console.log(`Example app listening on port : ${port}`)
+})
+
+app.get('/users', (req,res)=>
+    {
+
+        console.log("Query parameter recieve :", req.query)
+    
+        let users = [{
+            name : req.query.name,
+            rollno: req.query.rollno,
+            class: "BSSE-IV"
+        }]
+    
+        try {
+             res.status(201).json(
+                {
+                    data: users,
+                    status: "success"
+                }
+             );
+        } catch (error) {
+            res.status(401).json(
+                {
+                    data: [],
+                    status: "error"
+                }
+            )
+        }
+            
+    })
+    
