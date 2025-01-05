37911483a6d2e964d1d157f5209e382f3ad11327
fanubalti786 <fanubalti786@gmail.com>
2025-01-06 01:30:47 +0500

    update

--- Apiclass05/server.js
+++ Apiclass05/server.js
@@ -19,6 +19,41 @@ app.use(express.urlencoded({extended:false}))
 app.use(cors())
 app.use('/auth', AuthRouter)
 app.use('/product', ProductRouter)
+app.use('/verify', (req,res,next)=>
+{
+    const auth = req.headers['authorization'];
+    if(!auth)
+    {
+        return res.status(403).json({
+            message: "Unauthorized, jwt token is require"
+
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(auth, process.env.JWT_SECRET);
+        if(decoded)
+        {
+          return res.status(204).json({
+            success: true
+          })
+        }
+
+        if(!decoded)
+          {
+            return res.status(204).json({
+              success: false
+            })
+          }
+        
+        
+    } catch (error) {
+        return res.status(403).json({
+            message: "Unauthorized, jwt token is require",
+            success: false
+        });
+    }
+})
 
 // app.use("/api/v1",aaosdfj)
 
